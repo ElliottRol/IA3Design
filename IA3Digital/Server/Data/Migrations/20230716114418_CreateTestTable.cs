2d9534ec98a46606a8469507fb1a65df113f922a
Elliott Roll <elliott@digitalnemesis.com>
2023-07-16 21:56:28 +1000

    created EF migration for test data table

---
+++ IA3Digital/Server/Data/Migrations/20230716114418_CreateTestTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IA3Digital.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTestTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Table",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: true),
+                    TemperatureC = table.Column<int>(type: "int", nullable: true),
+                    TemperatureF = table.Column<int>(type: "int", nullable: true),
+                    Summary = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Table", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Table");
+        }
+    }
+}
